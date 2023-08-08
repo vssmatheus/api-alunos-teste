c5ecf0c17ec77f7783f64104fd0e190630546434
vssmatheus <vssmatheus@gmail.com>
2023-08-08 03:18:13 -0300

    cors-fix

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
